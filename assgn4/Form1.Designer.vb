967630dbd9c2979097fd09dbe06fc03f52b8132a
unknown <richard.hildred@FE-A008-PC41.acad.mc.local>
2015-11-26 13:47:22 -0500

    initial commit

---
+++ assgn4/Form1.Designer.vb
@@ -0,0 +1,375 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtName0 = New System.Windows.Forms.TextBox()
+        Me.txtCut0 = New System.Windows.Forms.TextBox()
+        Me.txtColor0 = New System.Windows.Forms.TextBox()
+        Me.txtUpdo0 = New System.Windows.Forms.TextBox()
+        Me.txtUpdo1 = New System.Windows.Forms.TextBox()
+        Me.txtColor1 = New System.Windows.Forms.TextBox()
+        Me.txtCut1 = New System.Windows.Forms.TextBox()
+        Me.txtName1 = New System.Windows.Forms.TextBox()
+        Me.txtUpdo2 = New System.Windows.Forms.TextBox()
+        Me.txtColor2 = New System.Windows.Forms.TextBox()
+        Me.txtCut2 = New System.Windows.Forms.TextBox()
+        Me.txtName2 = New System.Windows.Forms.TextBox()
+        Me.txtUpdo3 = New System.Windows.Forms.TextBox()
+        Me.txtColor3 = New System.Windows.Forms.TextBox()
+        Me.txtCut3 = New System.Windows.Forms.TextBox()
+        Me.txtName3 = New System.Windows.Forms.TextBox()
+        Me.txtUpdo4 = New System.Windows.Forms.TextBox()
+        Me.txtColor4 = New System.Windows.Forms.TextBox()
+        Me.txtCut4 = New System.Windows.Forms.TextBox()
+        Me.txtName4 = New System.Windows.Forms.TextBox()
+        Me.txtAvg2 = New System.Windows.Forms.TextBox()
+        Me.txtAvg1 = New System.Windows.Forms.TextBox()
+        Me.txtAvg0 = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(45, 55)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(253, 55)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(34, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Cut"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(391, 55)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(46, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Color"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(533, 55)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(48, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Updo"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(49, 338)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(152, 20)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Average Per Service"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(246, 371)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(101, 31)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtName0
+        '
+        Me.txtName0.Location = New System.Drawing.Point(49, 99)
+        Me.txtName0.Name = "txtName0"
+        Me.txtName0.Size = New System.Drawing.Size(100, 26)
+        Me.txtName0.TabIndex = 6
+        '
+        'txtCut0
+        '
+        Me.txtCut0.Location = New System.Drawing.Point(257, 99)
+        Me.txtCut0.Name = "txtCut0"
+        Me.txtCut0.Size = New System.Drawing.Size(100, 26)
+        Me.txtCut0.TabIndex = 7
+        '
+        'txtColor0
+        '
+        Me.txtColor0.Location = New System.Drawing.Point(395, 99)
+        Me.txtColor0.Name = "txtColor0"
+        Me.txtColor0.Size = New System.Drawing.Size(100, 26)
+        Me.txtColor0.TabIndex = 8
+        '
+        'txtUpdo0
+        '
+        Me.txtUpdo0.Location = New System.Drawing.Point(537, 99)
+        Me.txtUpdo0.Name = "txtUpdo0"
+        Me.txtUpdo0.Size = New System.Drawing.Size(100, 26)
+        Me.txtUpdo0.TabIndex = 9
+        '
+        'txtUpdo1
+        '
+        Me.txtUpdo1.Location = New System.Drawing.Point(541, 140)
+        Me.txtUpdo1.Name = "txtUpdo1"
+        Me.txtUpdo1.Size = New System.Drawing.Size(100, 26)
+        Me.txtUpdo1.TabIndex = 13
+        '
+        'txtColor1
+        '
+        Me.txtColor1.Location = New System.Drawing.Point(399, 140)
+        Me.txtColor1.Name = "txtColor1"
+        Me.txtColor1.Size = New System.Drawing.Size(100, 26)
+        Me.txtColor1.TabIndex = 12
+        '
+        'txtCut1
+        '
+        Me.txtCut1.Location = New System.Drawing.Point(261, 140)
+        Me.txtCut1.Name = "txtCut1"
+        Me.txtCut1.Size = New System.Drawing.Size(100, 26)
+        Me.txtCut1.TabIndex = 11
+        '
+        'txtName1
+        '
+        Me.txtName1.Location = New System.Drawing.Point(53, 140)
+        Me.txtName1.Name = "txtName1"
+        Me.txtName1.Size = New System.Drawing.Size(100, 26)
+        Me.txtName1.TabIndex = 10
+        '
+        'txtUpdo2
+        '
+        Me.txtUpdo2.Location = New System.Drawing.Point(541, 195)
+        Me.txtUpdo2.Name = "txtUpdo2"
+        Me.txtUpdo2.Size = New System.Drawing.Size(100, 26)
+        Me.txtUpdo2.TabIndex = 17
+        '
+        'txtColor2
+        '
+        Me.txtColor2.Location = New System.Drawing.Point(399, 195)
+        Me.txtColor2.Name = "txtColor2"
+        Me.txtColor2.Size = New System.Drawing.Size(100, 26)
+        Me.txtColor2.TabIndex = 16
+        '
+        'txtCut2
+        '
+        Me.txtCut2.Location = New System.Drawing.Point(261, 195)
+        Me.txtCut2.Name = "txtCut2"
+        Me.txtCut2.Size = New System.Drawing.Size(100, 26)
+        Me.txtCut2.TabIndex = 15
+        '
+        'txtName2
+        '
+        Me.txtName2.Location = New System.Drawing.Point(53, 195)
+        Me.txtName2.Name = "txtName2"
+        Me.txtName2.Size = New System.Drawing.Size(100, 26)
+        Me.txtName2.TabIndex = 14
+        '
+        'txtUpdo3
+        '
+        Me.txtUpdo3.Location = New System.Drawing.Point(541, 238)
+        Me.txtUpdo3.Name = "txtUpdo3"
+        Me.txtUpdo3.Size = New System.Drawing.Size(100, 26)
+        Me.txtUpdo3.TabIndex = 21
+        '
+        'txtColor3
+        '
+        Me.txtColor3.Location = New System.Drawing.Point(399, 238)
+        Me.txtColor3.Name = "txtColor3"
+        Me.txtColor3.Size = New System.Drawing.Size(100, 26)
+        Me.txtColor3.TabIndex = 20
+        '
+        'txtCut3
+        '
+        Me.txtCut3.Location = New System.Drawing.Point(261, 238)
+        Me.txtCut3.Name = "txtCut3"
+        Me.txtCut3.Size = New System.Drawing.Size(100, 26)
+        Me.txtCut3.TabIndex = 19
+        '
+        'txtName3
+        '
+        Me.txtName3.Location = New System.Drawing.Point(53, 238)
+        Me.txtName3.Name = "txtName3"
+        Me.txtName3.Size = New System.Drawing.Size(100, 26)
+        Me.txtName3.TabIndex = 18
+        '
+        'txtUpdo4
+        '
+        Me.txtUpdo4.Location = New System.Drawing.Point(541, 287)
+        Me.txtUpdo4.Name = "txtUpdo4"
+        Me.txtUpdo4.Size = New System.Drawing.Size(100, 26)
+        Me.txtUpdo4.TabIndex = 25
+        '
+        'txtColor4
+        '
+        Me.txtColor4.Location = New System.Drawing.Point(399, 287)
+        Me.txtColor4.Name = "txtColor4"
+        Me.txtColor4.Size = New System.Drawing.Size(100, 26)
+        Me.txtColor4.TabIndex = 24
+        '
+        'txtCut4
+        '
+        Me.txtCut4.Location = New System.Drawing.Point(261, 287)
+        Me.txtCut4.Name = "txtCut4"
+        Me.txtCut4.Size = New System.Drawing.Size(100, 26)
+        Me.txtCut4.TabIndex = 23
+        '
+        'txtName4
+        '
+        Me.txtName4.Location = New System.Drawing.Point(53, 287)
+        Me.txtName4.Name = "txtName4"
+        Me.txtName4.Size = New System.Drawing.Size(100, 26)
+        Me.txtName4.TabIndex = 22
+        '
+        'txtAvg2
+        '
+        Me.txtAvg2.Location = New System.Drawing.Point(541, 332)
+        Me.txtAvg2.Name = "txtAvg2"
+        Me.txtAvg2.Size = New System.Drawing.Size(100, 26)
+        Me.txtAvg2.TabIndex = 28
+        '
+        'txtAvg1
+        '
+        Me.txtAvg1.Location = New System.Drawing.Point(399, 332)
+        Me.txtAvg1.Name = "txtAvg1"
+        Me.txtAvg1.Size = New System.Drawing.Size(100, 26)
+        Me.txtAvg1.TabIndex = 27
+        '
+        'txtAvg0
+        '
+        Me.txtAvg0.Location = New System.Drawing.Point(261, 332)
+        Me.txtAvg0.Name = "txtAvg0"
+        Me.txtAvg0.Size = New System.Drawing.Size(100, 26)
+        Me.txtAvg0.TabIndex = 26
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(773, 33)
+        Me.MenuStrip1.TabIndex = 29
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SaveToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(50, 29)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(198, 30)
+        Me.SaveToolStripMenuItem.Text = "Save"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(773, 408)
+        Me.Controls.Add(Me.txtAvg2)
+        Me.Controls.Add(Me.txtAvg1)
+        Me.Controls.Add(Me.txtAvg0)
+        Me.Controls.Add(Me.txtUpdo4)
+        Me.Controls.Add(Me.txtColor4)
+        Me.Controls.Add(Me.txtCut4)
+        Me.Controls.Add(Me.txtName4)
+        Me.Controls.Add(Me.txtUpdo3)
+        Me.Controls.Add(Me.txtColor3)
+        Me.Controls.Add(Me.txtCut3)
+        Me.Controls.Add(Me.txtName3)
+        Me.Controls.Add(Me.txtUpdo2)
+        Me.Controls.Add(Me.txtColor2)
+        Me.Controls.Add(Me.txtCut2)
+        Me.Controls.Add(Me.txtName2)
+        Me.Controls.Add(Me.txtUpdo1)
+        Me.Controls.Add(Me.txtColor1)
+        Me.Controls.Add(Me.txtCut1)
+        Me.Controls.Add(Me.txtName1)
+        Me.Controls.Add(Me.txtUpdo0)
+        Me.Controls.Add(Me.txtColor0)
+        Me.Controls.Add(Me.txtCut0)
+        Me.Controls.Add(Me.txtName0)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Profits by service"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents txtName0 As System.Windows.Forms.TextBox
+    Friend WithEvents txtCut0 As System.Windows.Forms.TextBox
+    Friend WithEvents txtColor0 As System.Windows.Forms.TextBox
+    Friend WithEvents txtUpdo0 As System.Windows.Forms.TextBox
+    Friend WithEvents txtUpdo1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtColor1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtCut1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtName1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtUpdo2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtColor2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtCut2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtName2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtUpdo3 As System.Windows.Forms.TextBox
+    Friend WithEvents txtColor3 As System.Windows.Forms.TextBox
+    Friend WithEvents txtCut3 As System.Windows.Forms.TextBox
+    Friend WithEvents txtName3 As System.Windows.Forms.TextBox
+    Friend WithEvents txtUpdo4 As System.Windows.Forms.TextBox
+    Friend WithEvents txtColor4 As System.Windows.Forms.TextBox
+    Friend WithEvents txtCut4 As System.Windows.Forms.TextBox
+    Friend WithEvents txtName4 As System.Windows.Forms.TextBox
+    Friend WithEvents txtAvg2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtAvg1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtAvg0 As System.Windows.Forms.TextBox
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ assgn4/Form1.vb
@@ -0,0 +1,55 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private aPricesGrid(,) As TextBox
+    Private aAverages() As TextBox
+    Private aNames() As TextBox
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        aPricesGrid = {{txtCut0, txtColor0, txtUpdo0},
+                       {txtCut1, txtColor1, txtUpdo1},
+                       {txtCut2, txtColor2, txtUpdo2},
+                       {txtCut3, txtColor3, txtUpdo3},
+                       {txtCut4, txtColor4, txtUpdo4}}
+        aAverages = {txtAvg0, txtAvg1, txtAvg2}
+        aNames = {txtName0, txtName1, txtName2, txtName3, txtName4}
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        For nCol As Integer = 0 To 2
+            Dim nColTotal As Integer = 0
+            Dim nColItems As Integer = 0
+            For nRow As Integer = 0 To 4
+                Try
+                    nColTotal += aPricesGrid(nRow, nCol).Text
+                    nColItems += 1
+                Catch ex As Exception
+
+                End Try
+            Next
+            aAverages(nCol).Text = nColTotal / nColItems
+
+        Next
+
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        Dim oSaveFileDialog As New SaveFileDialog()
+        'Display the Common file Dialopg to user 
+        oSaveFileDialog.Filter = "Text Files (*.txt)|*.txt"
+        If oSaveFileDialog.ShowDialog() = Windows.Forms.DialogResult.OK Then
+
+            ' Retrieve Name and open it 
+            Dim fileName As String = oSaveFileDialog.FileName
+            Dim outputFile As StreamWriter = File.CreateText(fileName)
+            For nRow As Integer = 0 To 4
+                outputFile.Write(aNames(nRow).Text)
+                For nCol As Integer = 0 To 2
+                    outputFile.Write(", " & aPricesGrid(nRow, nCol).Text)
+                Next
+                outputFile.WriteLine()
+            Next
+            outputFile.Close()
+        End If
+    End Sub
+End Class
